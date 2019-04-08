7f1e799d4a0917b556db3a38b6285828c0c25f0e
Victorz <victorzsnail@qq.com>
2019-04-08 22:06:08 +0800

    SparkStreaming + Flume

---
+++ src/main/java/SparkStreamingFlume/FlumePushWordCount.scala
@@ -0,0 +1,32 @@
+package SparkStreamingFlume
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.flume.FlumeUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * Created by dell on 2019/4/8.
+  */
+object FlumePushWordCount {
+  def main(args: Array[String]): Unit = {
+
+    if(args.length != 2) {
+      System.err.println("Usage: FlumePushWordCount <hostname> <port>")
+      System.exit(1)
+    }
+
+    val Array(hostname, port) = args
+
+    val sparkConf = new SparkConf() //.setMaster("local[2]").setAppName("FlumePushWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    //使用SparkStreaming整合Flume
+    val flumeStream = FlumeUtils.createStream(ssc, "127.0.0.1", 41414)
+
+    flumeStream.map(x=> new String(x.event.getBody.array()).trim)
+      .flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
